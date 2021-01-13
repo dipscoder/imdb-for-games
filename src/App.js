dc88d6e290c194d41bb00d352d767cba1124af30
dipscoder <dipeshjaiswal140@gmail.com>
2021-01-13 16:20:03 +0530

    Add Global Styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+
+// Components and Pages
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 
 
 function App() {
 
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
