b8bef671553292693b0051f3b92f23d3df6d94e3
dipscoder <dipeshjaiswal140@gmail.com>
2021-01-23 23:24:13 +0530

    Add platform logos

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,6 +6,13 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { smallImage } from "../util";
+// Images
+import playstation from "../img/playstation.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
+import nintendo from "../img/nintendo.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
 
 const GameDetail = ({ pathId }) => {
   // Exit Detail Page
@@ -23,6 +30,26 @@ const GameDetail = ({ pathId }) => {
   
   //Data
   const { gameDetail, screenshots, isLoading } = useSelector((state) => state.detail);
+
+  // Rendering Platform logos
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 5":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+    
+      default:
+        return gamepad;
+    }
+  }
+
   return (
     <>
       { !isLoading && (
@@ -37,7 +64,7 @@ const GameDetail = ({ pathId }) => {
               <h3>Platforms</h3>
               <Platforms>
                 {gameDetail.platforms.map((data) => (
-                  <h3 key={data.platform.id}>{data.platform.name}</h3>
+                  <img key={data.platform.id} alt={data.platform.name} src={getPlatform(data.platform.name)} />
                   ))}
               </Platforms>
             </Info>
